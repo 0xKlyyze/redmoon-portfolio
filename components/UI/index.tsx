7fc60bd5b50c75f7c034bbdcd1bd847037b87814
0xKlyyze <mpitaval07@gmail.com>
2026-01-21 12:09:05 +0900

    deploy again

--- components/UI/index.tsx
+++ components/UI/index.tsx
@@ -10,7 +10,7 @@ import { useAppStore } from "@/store/useAppStore";
 export default function UIOverlay() {
     const fetchAsteroids = useAppStore((state) => state.fetchAsteroids);
 
-    // Fetch asteroids from API on mount
+    // Fetch asteroids from API on mount ()
     useEffect(() => {
         fetchAsteroids();
     }, [fetchAsteroids]);
